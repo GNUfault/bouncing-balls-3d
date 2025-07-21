2b97bfa41846793ac71c2481d180d32d591298d3
Connor Thomson <blumatrikz@gmail.com>
2025-07-21 13:28:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# bouncing-balls-3d - Bouncing Balls in 3D made with Pygame in Python
+# Copyright (C) 2025  Connor Thomson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
